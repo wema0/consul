fbedfe2e76541c11cd4aeb9a79d7718b754a4b9b
voodoorai2000 <voodoorai2000@gmail.com>
2019-03-28 01:16:34 +0100

    Add relation between ballots and balllot lines

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -17,6 +17,7 @@ class Budget < ActiveRecord::Base
 
   has_many :investments, dependent: :destroy
   has_many :ballots, dependent: :destroy
+  has_many :lines, through: :ballots, class_name: 'Budget::Ballot::Line'
   has_many :groups, dependent: :destroy
   has_many :headings, through: :groups
   has_many :phases, class_name: Budget::Phase
