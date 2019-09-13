4100a2c5aea9035a839a54439cfee77bc9a721f4
Javi Martín <javim@elretirao.net>
2019-09-13 17:40:13 +0200

    Prevent AJAX requests to anchor links
    Turbolinks 5 doesn't follow the browser's standard behaviour of ignoring
    links pointing to "#", so we're preventing the turbolinks events in this
    situation:
    
    https://github.com/turbolinks/turbolinks/issues/75

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require jquery-fileupload/basic
 //= require foundation
 //= require turbolinks
+//= require turbolinks_anchors
 //= require ckeditor/loader
 //= require_directory ./ckeditor
 //= require social-share-button
